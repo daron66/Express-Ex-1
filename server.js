9efd6447ab71ecf7d22db38f858b5776e3053414
Daron Andersonn <66185223+daron66@users.noreply.github.com>
2020-08-02 23:44:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ server.use(express.json());
 //used fort route handler function
 //which sets json
 try {
-	server.get('/', (req, res) => {
-	res.json({message: 'OK'});
-	});
+        server.get('/', (req, res) => {
+        res.json({message: 'OK'});
+        });
+        server.listen(3333, () => console.log('System is listening on port 127.0.0.1:3000'))
+
 } catch (err) {
-	console.error(err.message);
+        console.error(err.message);
 }
 
-server.listen(3333, () => console.log('System is listening on port 127.0.0.1:3000'))
 
